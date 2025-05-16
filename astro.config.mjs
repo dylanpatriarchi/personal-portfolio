82b808a43cba862db23fb4d61432cc19411e1381
dylan patriarchi <dylanpat2004@icloud.com>
2025-05-16 10:47:15 +0200

    SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.patriarchidylan.it',
   vite: {
     plugins: [tailwindcss()]
   },
   output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  integrations: [
+    sitemap()
+  ]
 });
